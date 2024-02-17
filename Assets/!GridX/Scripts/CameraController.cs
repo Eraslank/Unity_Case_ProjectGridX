225d691e93e0792776dddfb662293a6ecd7bd809
Eraslank <kayraeraslan98@gmail.com>
2024-02-17 06:14:44 +0300

    Camera Scaling Fix

--- Assets/!GridX/Scripts/CameraController.cs
+++ Assets/!GridX/Scripts/CameraController.cs
@@ -24,24 +24,10 @@ public class CameraController : MonoBehaviourSingleton<CameraController>
         Grid.OnGenerated -= OnGridGenerated;
     }
 
-    public void OnGridGenerated(int size)
+    public void OnGridGenerated(int gridSize)
     {
-        //var ortoSize = (float)size * 3000 / (float)cam.pixelHeight;
-        var aspect = cam.aspect < 0.5f ? cam.aspect + cam.aspect : cam.aspect;
-        Debug.Log(cam.pixelRect);
-        Debug.Log(cam.aspect);
-        Debug.Log(aspect);
-        //cam.orthographicSize = ortoSize;
+        var scaleRatio = scaleFactor * cam.aspect;
 
-
-
-
-        var reference = new Rect(0, 0, 1080, 1920);
-        var ortoSize = (cam.pixelWidth / reference.width) /*+ (cam.pixelHeight / reference.height)*/;
-        var scaleRatio = .6f;
-        if (cam.aspect < .5f)
-            scaleRatio += 1;
-
-        cam.orthographicSize = (ortoSize * (size / 2f)) * scaleFactor * scaleRatio;
+        cam.orthographicSize = gridSize / scaleRatio;
     }
 }
